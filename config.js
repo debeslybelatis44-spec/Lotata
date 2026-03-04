eb5e81636248d37383ffa3616b3b5ab94f44d9f4
debeslybelatis44-spec <debeslybelatis44@gmail.com>
2026-03-04 18:11:35 -0500

    Update config.js

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ const CONFIG = {
         { id: 'fl_matin', name: 'Florida Matin', time: '13:30', color: 'var(--florida)' },
         { id: 'fl_soir', name: 'Florida Soir', time: '21:50', color: 'var(--florida)' },
         { id: 'ny_matin', name: 'New York Matin', time: '14:30', color: 'var(--newyork)' },
-        { id: 'ny_soir', name: 'New York Soir', time: '20:00', color: 'var(--newyork)' },
+        { id: 'ny_soir', name: 'New York Soir', time: '22:28', color: 'var(--newyork)' },
         { id: 'ga_matin', name: 'Georgia Matin', time: '12:30', color: 'var(--georgia)' },
         { id: 'ga_soir', name: 'Georgia Soir', time: '19:00', color: 'var(--georgia)' },
         { id: 'tx_matin', name: 'Texas Matin', time: '11:30', color: 'var(--texas)' },
